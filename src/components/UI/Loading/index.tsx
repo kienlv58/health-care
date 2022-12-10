c4f8989100845794a50d1f51a0f09575be160078
Kien <klo@meetcortex.com>
2022-12-10 09:14:26 +0700

    Fix: update UI loading

--- src/components/UI/Loading/index.tsx
+++ src/components/UI/Loading/index.tsx
@@ -1,5 +1,9 @@
 import styles from "./Loading.module.scss";
 const Loading = () => {
-  return <div className={styles.loading}>loading ....</div>;
+  return (
+    <div className={styles.loading}>
+      <div className={styles.loader}></div>
+    </div>
+  );
 };
 export default Loading;
